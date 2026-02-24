db28bc769ae1e743c32f7023b7b2692b56aa4b8d
PacomeDutreuil <13007@holbertonstudents.com>
2026-02-24 15:28:28 +0100

    Add 3-islower.c script

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,32 +1,15 @@
 #include "main.h"
-/**
- * returns 1 if c is lowercase
- * returns 0 otherwise
- */
-int _islower(int c)
-{
-	char b = 'c'
-
-	if (_islower(b))
-	{
-	return (1);
-	}
-	else
-	{
-	return (0);
-	}
-}
-#include "main.h"  // si tu as un header, sinon tu peux juste mettre le prototype en haut
 
 /**
- * _islower - vérifie si un caractère est en minuscule
- * @c: caractère à vérifier
- * Return: 1 si minuscule, 0 sinon
+ * _islower - checks for lowercase character
+ * @c: character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
     if (c >= 'a' && c <= 'z')
-        return (1);
+        return 1;
     else
-        return (0);
+        return 0;
 }
