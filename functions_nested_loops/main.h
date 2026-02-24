227e11dd2bbb78cb9daa81fae9a8f34eac514ac5
PacomeDutreuil <13007@holbertonstudents.com>
2026-02-24 11:25:13 +0100

    add main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,26 +1,7 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <ctype.h>
-
-int _isupper(int c);
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
+void print_alphabet(void);
 
-int _isdigit(int c);
-
-int mul(int a, int b);
-
-void print_numbers(void);
-
-void print_most_numbers(void);
-
-void more_numbers(void);
-
-void print_line(int n);
-
-void print_diagonal(int n);
-
-void print_square(int size);
-
-void print_triangle(int size);
+#endif /* MAIN_H */
