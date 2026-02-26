95b9460a4efb7caaac8091859a705963a23a7cce
PacomeDutreuil <13007@holbertonstudents.com>
2026-02-26 10:39:24 +0100

    Add calculator.c script

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,8 +1,20 @@
-Calculatrice simple
-1) Ajouter
-2) Soustraire
-3) Multiplier
-4) Diviser
-0) Quitter
-Choix : 0
-Au revoir !
+#include <stdio.h>
+
+int main(void)
+{
+    int choix;
+
+    printf("Calculatrice simple\n");
+    printf("1) Ajouter\n");
+    printf("2) Soustraire\n");
+    printf("3) Multiplier\n");
+    printf("4) Diviser\n");
+    printf("0) Quitter\n");
+    printf("Choix : ");
+    scanf("%d", &choix);
+
+    if (choix == 0)
+        printf("Au revoir !\n");
+
+    return 0;
+}
