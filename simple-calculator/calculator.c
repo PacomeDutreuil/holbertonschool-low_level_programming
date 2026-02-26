8e6af76db0f69d6d646a0987ed1acd0ce0e091ea
PacomeDutreuil <13007@holbertonstudents.com>
2026-02-26 10:55:43 +0100

    Add calculator.c script

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -3,18 +3,70 @@
 int main(void)
 {
     int choix;
+    int A, B;
 
-    printf("Calculatrice simple\n");
-    printf("1) Ajouter\n");
-    printf("2) Soustraire\n");
-    printf("3) Multiplier\n");
-    printf("4) Diviser\n");
-    printf("0) Quitter\n");
-    printf("Choix : ");
-    scanf("%d", &choix);
+    do
+    {
+        printf("Calculatrice simple\n");
+        printf("1) Ajouter\n");
+        printf("2) Soustraire\n");
+        printf("3) Multiplier\n");
+        printf("4) Diviser\n");
+        printf("0) Quitter\n");
+        printf("Choix : ");
+        scanf("%d", &choix);
 
-    if (choix == 0)
-        printf("Au revoir !\n");
+        if (choix == 1)
+        {
+            printf("A : ");
+            scanf("%d", &A);
+            printf("B : ");
+            scanf("%d", &B);
+            printf("Résultat : %d\n", A + B);
+        }
+        else if (choix == 2)
+        {
+            printf("A : ");
+            scanf("%d", &A);
+            printf("B : ");
+            scanf("%d", &B);
+            printf("Résultat : %d\n", A - B);
+        }
+        else if (choix == 3)
+        {
+            printf("A : ");
+            scanf("%d", &A);
+            printf("B : ");
+            scanf("%d", &B);
+            printf("Résultat : %d\n", A * B);
 
+        }
+	else if (choix == 4)
+	{
+	    int B;
+	    float resultat;
+	    printf("A : ");
+	    scanf("%d", &A);
+	    printf("B : ");
+	    scanf("%d", &B);
+	
+	    if (B == 0)
+	 {
+        printf("Erreur : division par zéro\n");
+	 }
+    else
+    {
+        resultat = (float)A / B;
+        printf("Résultat : %.2f\n", resultat);
+    }
+}	
+        else if (choix != 0)
+        {
+            printf("Choix invalide\n");
+        }
+
+    } while (choix != 0);
+
+    printf("Au revoir !\n");
     return 0;
 }
