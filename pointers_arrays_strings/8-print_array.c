3c39b70e37d3e1b272281de8c421cd9cfdbf793f
PacomeDutreuil <13007@holbertonstudents.com>
2026-03-03 16:41:13 +0100

    Add 8-print_array.c script

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_array - print n elements of an array of integers
- * @a: pointer to the array
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to array
  * @n: number of elements to print
- * followed by new line
  */
 void print_array(int *a, int n)
 {
-int i;
+    int i;
 
-for (i = 0; i < n; i++)
-{
-printf("%d", a[i]);
-if (i < n - 1)
-printf(", ");
-}
-printf("\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+
+        if (i < n - 1)
+            printf(", ");
+    }
+
+    printf("\n");
 }
