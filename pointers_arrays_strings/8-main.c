3ca2d377129a6ac65d385ea7c206c558e8b21faf
PacomeDutreuil <13007@holbertonstudents.com>
2026-03-03 16:39:56 +0100

    Add 8-main.c

--- pointers_arrays_strings/8-main.c
+++ pointers_arrays_strings/8-main.c
@@ -2,20 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_array - print n elements of an array of integers
- * @a: pointer to the array
- * @n: number of elements to print
- * followed by new line
+ * main - check the code for
+ *
+ * Return: Always 0.
  */
-void print_array(int *a, int n)
+int main(void)
 {
-int i;
+    int array[5];
 
-for (i = 0; i < n; i++)
-{
-printf("%d", a[i]);
-if (i < n - 1)
-printf(", ");
-}
-printf("\n");
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
+    return (0);
 }
