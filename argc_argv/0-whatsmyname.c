028c1c5cebf7245236071a3aec757feb52fab0e5
PacomeDutreuil <13007@holbertonstudents.com>
2026-03-05 22:26:54 +0100

    Add 0-whatsmyname.c script

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of the program
+ * @argc: Argument count
+ * @argv: Argument vector (array of strings)
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
