a89a0c3c19a593aee86ca7e661eea8d36c6ff2ea
PacomeDutreuil <13007@holbertonstudents.com>
2026-03-05 10:29:58 +0100

    Add 4-strpbrk.c script

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,30 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * _strpbrk - Searches a string for any of a set of bytes
- * @s: The main string to be scanned
- * @accept: The string containing the characters to match
- *
- * Return: A pointer to the first matching character in s, or NULL if none
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to scan
+ * @accept: characters to match
+ * Return: pointer to the byte in s that matches one in accept, or NULL
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
 	while (*s)
 	{
-	for (i = 0; accept[i]; i++)
-	{
-	if (*s == accept[i])
-	return (s);
-	}
-	s++;
+		i = 0;
+
+		while (accept[i])
+		{
+			if (*s == accept[i])
+				return (s);
+			i++;
+		}
+
+		s++;
 	}
 
 	return (NULL);
 }
-
