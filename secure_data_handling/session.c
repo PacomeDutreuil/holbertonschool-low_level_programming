1e87e4bae82fddba7684138f968bc3db887cbc50
PacomeDutreuil <13007@holbertonstudents.com>
2026-04-02 10:32:39 +0200

    Add all fils for secure_data_handling project

--- secure_data_handling/session.c
+++ secure_data_handling/session.c
@@ -4,65 +4,71 @@
 
 session_t *session_create(const char *id, unsigned int uid, const unsigned char *data, size_t data_len)
 {
-	session_t *s;
+    session_t *s;
 
-	s = (session_t *)malloc(sizeof(*s));
-	if (!s)
-		return NULL;
+    if (!id)
+        return NULL;
 
-	s->id = (char *)id;
+    s = malloc(sizeof(*s));
+    if (!s)
+        return NULL;
 
-	s->uid = uid;
+    s->id = strdup(id);
+    if (!s->id) {
+        free(s);
+        return NULL;
+    }
 
-	if (data_len > 0) {
-		s->data = (unsigned char *)malloc(data_len);
-		if (!s->data) {
-			return NULL;
-		}
-		memcpy(s->data, data, data_len);
-		s->data_len = data_len;
-	} else {
-		s->data = NULL;
-		s->data_len = 0;
-	}
+    s->uid = uid;
 
-	return s;
+    if (data_len > 0) {
+        s->data = malloc(data_len);
+        if (!s->data) {
+            free(s->id);
+            free(s);
+            return NULL;
+        }
+        memcpy(s->data, data, data_len);
+        s->data_len = data_len;
+    } else {
+        s->data = NULL;
+        s->data_len = 0;
+    }
+
+    return s;
 }
 
 int session_set_data(session_t *s, const unsigned char *data, size_t data_len)
 {
-	unsigned char *tmp;
+    unsigned char *tmp;
 
-	if (!s)
-		return 0;
+    if (!s)
+        return 0;
 
-	if (data_len == 0) {
-		free(s->data);
-		s->data = NULL;
-		s->data_len = 0;
-		return 1;
-	}
+    if (data_len == 0) {
+        free(s->data);
+        s->data = NULL;
+        s->data_len = 0;
+        return 1;
+    }
 
-	tmp = (unsigned char *)realloc(s->data, data_len);
-	s->data = tmp;
+    tmp = realloc(s->data, data_len);
+    if (!tmp)
+        return 0;
 
-	if (!s->data) {
-		s->data_len = 0;
-		return 0;
-	}
+    s->data = tmp;
+    memcpy(s->data, data, data_len);
+    s->data_len = data_len;
 
-	memcpy(s->data, data, data_len);
-	s->data_len = data_len;
-	return 1;
+    return 1;
 }
 
 void session_destroy(session_t *s)
 {
-	if (!s)
-		return;
+    if (!s)
+        return;
 
-	free(s->id);
-
-	free(s->data);
-	free(s);
+    free(s->id);
+    free(s->data);
+    free(s);
 }
